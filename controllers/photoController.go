813a0054fa3a9e461b774cff4f5a7b1dcd1a230c
Rian Febriansyah <rianfebriansyah22@gmail.com>
2023-04-10 21:44:54 +0700

    feat: add photo endpoints

---
+++ controllers/photoController.go
@@ -0,0 +1,149 @@
+package controllers
+
+import (
+	"my-gram/database"
+	"my-gram/helpers"
+	"my-gram/models"
+	"net/http"
+	"strconv"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func CreatePhoto(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	contentType := helpers.GetContentType(c)
+
+	Photo := models.Photo{}
+	userID := uint(userData["id"].(float64))
+
+	if contentType == appJSON {
+		c.ShouldBindJSON(&Photo)
+	} else {
+		c.ShouldBind(&Photo)
+	}
+
+	Photo.UserID = userID
+
+	err := db.Debug().Create(&Photo).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, Photo)
+}
+
+func UpdatePhoto(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	contentType := helpers.GetContentType(c)
+	Photo := models.Photo{}
+
+	photoId, _ := strconv.Atoi(c.Param("photoId"))
+	userID := uint(userData["id"].(float64))
+
+	if contentType == appJSON {
+		c.ShouldBindJSON(&Photo)
+	} else {
+		c.ShouldBind(&Photo)
+	}
+
+	Photo.UserID = userID
+	Photo.ID = uint(photoId)
+
+	err := db.Model(&Photo).
+		Where("id = ?", photoId).
+		Updates(models.Photo{Title: Photo.Title, Caption: Photo.Caption, PhotoUrl: Photo.PhotoUrl}).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, Photo)
+}
+
+func GetPhotoById(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	Photo := models.Photo{}
+
+	photoId, _ := strconv.Atoi(c.Param("photoId"))
+	userID := uint(userData["id"].(float64))
+
+	Photo.UserID = userID
+	Photo.ID = uint(photoId)
+
+	err := db.First(&Photo, "id = ?", photoId).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, Photo)
+}
+
+func DeletePhotoById(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	Photo := models.Photo{}
+
+	photoId, _ := strconv.Atoi(c.Param("photoId"))
+	userID := uint(userData["id"].(float64))
+
+	Photo.UserID = userID
+	Photo.ID = uint(photoId)
+
+	result := db.Where("id = ?", photoId).Delete(&Photo)
+
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Data not found",
+			"message": "Data with selected id is not found",
+		})
+		return
+	}
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": result.Error.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Photo successfully deleted",
+	})
+}
+
+func GetAllPhotos(c *gin.Context) {
+	db := database.GetDB()
+	products := []models.Photo{}
+
+	err := db.Find(&products).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, products)
+}
--- router/router.go
+++ router/router.go
@@ -16,7 +16,7 @@ func StartApp() *gin.Engine {
 		userRouter.POST("/login", controllers.UserLogin)
 	}
 
-	socialMediaRouter := r.Group("/social-media")
+	socialMediaRouter := r.Group("/social-medias")
 	{
 		socialMediaRouter.Use(middlewares.Authentication())
 		socialMediaRouter.POST("/", controllers.CreateSocialMedia)
@@ -24,6 +24,16 @@ func StartApp() *gin.Engine {
 		socialMediaRouter.GET("/:socialMediaId", middlewares.SocialMediaAuthorization(), controllers.GetSocialMediaById)
 		socialMediaRouter.PUT("/:socialMediaId", middlewares.SocialMediaAuthorization(), controllers.UpdateSocialMedia)
 		socialMediaRouter.DELETE("/:socialMediaId", middlewares.SocialMediaAuthorization(), controllers.DeleteSocialMediaById)
+	}
+
+	photoRouter := r.Group("/photos")
+	{
+		photoRouter.Use(middlewares.Authentication())
+		photoRouter.POST("/", controllers.CreatePhoto)
+		photoRouter.GET("/", controllers.GetAllPhotos)
+		photoRouter.GET("/:photoId", middlewares.PhotoAuthorization(), controllers.GetPhotoById)
+		photoRouter.PUT("/:photoId", middlewares.PhotoAuthorization(), controllers.UpdatePhoto)
+		photoRouter.DELETE("/:photoId", middlewares.PhotoAuthorization(), controllers.DeletePhotoById)
 	}
 
 	return r
