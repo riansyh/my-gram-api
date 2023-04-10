8a31c7eb3f2aeac781c8f693b2223d01ecd21c71
Rian Febriansyah <rianfebriansyah22@gmail.com>
2023-04-10 22:07:35 +0700

    feat: add comment endpoints

---
+++ controllers/commentController.go
@@ -0,0 +1,160 @@
+package controllers
+
+import (
+	"my-gram/database"
+	"my-gram/helpers"
+	"my-gram/models"
+	"net/http"
+	"strconv"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateComment(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	contentType := helpers.GetContentType(c)
+
+	Comment := models.Comment{}
+	Photo := models.Photo{}
+	userID := uint(userData["id"].(float64))
+
+	if contentType == appJSON {
+		c.ShouldBindJSON(&Comment)
+	} else {
+		c.ShouldBind(&Comment)
+	}
+
+	Comment.UserID = userID
+
+	err := db.First(&Photo, "id = ?", Comment.PhotoId).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": "Photo not found",
+		})
+		return
+	}
+
+	err = db.Debug().Create(&Comment).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, Comment)
+}
+
+func UpdateComment(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	contentType := helpers.GetContentType(c)
+	Comment := models.Comment{}
+
+	commentId, _ := strconv.Atoi(c.Param("commentId"))
+	userID := uint(userData["id"].(float64))
+
+	if contentType == appJSON {
+		c.ShouldBindJSON(&Comment)
+	} else {
+		c.ShouldBind(&Comment)
+	}
+
+	Comment.UserID = userID
+	Comment.ID = uint(commentId)
+
+	err := db.Model(&Comment).
+		Where("id = ?", commentId).
+		Updates(models.Comment{Message: Comment.Message}).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, Comment)
+}
+
+func GetCommentById(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	Comment := models.Comment{}
+
+	commentId, _ := strconv.Atoi(c.Param("commentId"))
+	userID := uint(userData["id"].(float64))
+
+	Comment.UserID = userID
+	Comment.ID = uint(commentId)
+
+	err := db.First(&Comment, "id = ?", commentId).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, Comment)
+}
+
+func DeleteCommentById(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	Comment := models.Comment{}
+
+	commentId, _ := strconv.Atoi(c.Param("commentId"))
+	userID := uint(userData["id"].(float64))
+
+	Comment.UserID = userID
+	Comment.ID = uint(commentId)
+
+	result := db.Where("id = ?", commentId).Delete(&Comment)
+
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Data not found",
+			"message": "Data with selected id is not found",
+		})
+		return
+	}
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": result.Error.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Comment successfully deleted",
+	})
+}
+
+func GetAllComments(c *gin.Context) {
+	db := database.GetDB()
+	products := []models.Comment{}
+
+	err := db.Find(&products).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, products)
+}
--- router/router.go
+++ router/router.go
@@ -21,7 +21,7 @@ func StartApp() *gin.Engine {
 		socialMediaRouter.Use(middlewares.Authentication())
 		socialMediaRouter.POST("/", controllers.CreateSocialMedia)
 		socialMediaRouter.GET("/", controllers.GetAllSocialMedias)
-		socialMediaRouter.GET("/:socialMediaId", middlewares.SocialMediaAuthorization(), controllers.GetSocialMediaById)
+		socialMediaRouter.GET("/:socialMediaId", controllers.GetSocialMediaById)
 		socialMediaRouter.PUT("/:socialMediaId", middlewares.SocialMediaAuthorization(), controllers.UpdateSocialMedia)
 		socialMediaRouter.DELETE("/:socialMediaId", middlewares.SocialMediaAuthorization(), controllers.DeleteSocialMediaById)
 	}
@@ -31,9 +31,19 @@ func StartApp() *gin.Engine {
 		photoRouter.Use(middlewares.Authentication())
 		photoRouter.POST("/", controllers.CreatePhoto)
 		photoRouter.GET("/", controllers.GetAllPhotos)
-		photoRouter.GET("/:photoId", middlewares.PhotoAuthorization(), controllers.GetPhotoById)
+		photoRouter.GET("/:photoId", controllers.GetPhotoById)
 		photoRouter.PUT("/:photoId", middlewares.PhotoAuthorization(), controllers.UpdatePhoto)
 		photoRouter.DELETE("/:photoId", middlewares.PhotoAuthorization(), controllers.DeletePhotoById)
+	}
+
+	commentRouter := r.Group("/comments")
+	{
+		commentRouter.Use(middlewares.Authentication())
+		commentRouter.POST("/", controllers.CreateComment)
+		commentRouter.GET("/", controllers.GetAllComments)
+		commentRouter.GET("/:commentId", controllers.GetCommentById)
+		commentRouter.PUT("/:commentId", middlewares.CommentAuthorization(), controllers.UpdateComment)
+		commentRouter.DELETE("/:commentId", middlewares.CommentAuthorization(), controllers.DeleteCommentById)
 	}
 
 	return r
